092e5828b3ff170c131ef63d8571945ec780c12f
大森星 <oomori.akari7766@gmail.com>
2018-10-08 05:39:26 +0900

    プレイヤーの移動を実装

--- Assets/Carly/C_Scripts/Data/PlayerParameter.cs
+++ Assets/Carly/C_Scripts/Data/PlayerParameter.cs
@@ -8,7 +8,14 @@ public class PlayerParameter {
 	public bool isAction;   //trueの場合、プレイヤーの射撃、移動を制限する
 	public bool doNotTrun;   //trueの場合、砲台の動きを制限する
 
-	Material material;   //プレイヤーオブジェクトのマテリアル
+	public Material material;   //プレイヤーオブジェクトのマテリアル
+
+	public float moveSpeed, accelSpeed;
 
 	public Vector3 moveVel, oldVel;   //プレイヤーの移動量
+
+	public void ResetVel() {
+		oldVel = moveVel;
+		moveVel = new Vector3(0,0,0);
+	}
 }
--- Assets/Carly/C_Scripts/PlayerController.cs
+++ Assets/Carly/C_Scripts/PlayerController.cs
@@ -58,43 +58,44 @@ public class PlayerController : CustomMonoBehaviour {
 #region Controls
 	//砲台の回転処理
 	private void TurretTurn() {
-		//接続されているジョイコンの数だけ処理をする
 		for(int i = 0; i < joycons.Count; i++) {
-			//操作するジョイコンと砲台を配列から持ってくる
-			Turret selectTurret = turrets[i];
-			Joycon selectJoycon = joycons[i];
+			Turret inProcTurret = turrets[i];
+			Joycon inProcJoycon = joycons[i];
 
-			//砲台の角度をラジアン角で加算
-			selectTurret.rotation += selectJoycon.GetGyro().z * selectTurret.trunSpeed * Time.fixedDeltaTime;
+			//砲台の角度を加算
+			inProcTurret.rotation += inProcJoycon.GetGyro().z * inProcTurret.trunSpeed * Time.fixedDeltaTime;
 
 			//角度、砲台からプレイヤーまでの距離を元に砲台の位置を計算
-			float x = Mathf.Cos(-selectTurret.rotation) * selectTurret.trunRadius * transform.localScale.x;
-			float y = Mathf.Sin(-selectTurret.rotation) * selectTurret.trunRadius * transform.localScale.y;
+			float x = Mathf.Cos(-inProcTurret.rotation) * inProcTurret.trunRadius * transform.localScale.x;
+			float y = Mathf.Sin(-inProcTurret.rotation) * inProcTurret.trunRadius * transform.localScale.y;
 
 			//計算した位置に砲台を移動
-			selectTurret.transform.SetPosition(new Vector3(x,y) + transform.position);
+			inProcTurret.transform.SetPosition(new Vector3(x,y) + transform.position);
 
 			//プレイヤーから砲台までの方向ベクトルを計算して正規化
-			Vector2 lookDir = (selectTurret.transform.position - transform.position).normalized;
+			Vector2 lookDir = (inProcTurret.transform.position - transform.position).normalized;
 
 			//方向ベクトルを元に砲台の向きを変える
-			selectTurret.transform.LookAt2D(lookDir);
+			inProcTurret.transform.LookAt2D(lookDir);
 		}
 	}
 
 	//プレイヤーの移動
 	private void PlayerMove() {
 		for(int i = 0; i < joycons.Count; i++) {
-			Joycon selectJoycon = joycons[i];
+			Joycon inProcJoycon = joycons[i];
 
-			//プレイヤーの移動速度を設定
-			/*parameter.moveVel = new Vector3(selectJoycon.GetStick()[0], selectJoycon.GetStick()[1]) / joycons.Count;
-			if(joycons.Count == i + 1){
-			rigidbody2D.velocity = EaseLerp.Lin(parameter.moveVel, parameter.oldVel, 2f);
-			parameter.oldVel = parameter.moveVel;
-			}*/
+			//スティックの移動量から移動量を宣言
+			Vector2 inProcVel = new Vector2(inProcJoycon.GetStick()[0], inProcJoycon.GetStick()[1]) * parameter.moveSpeed;
+
+			//宣言した移動量と前の移動量を補間にかけ、MoveVelに加算
+			parameter.moveVel += EasingLerps.OutQuint(parameter.oldVel, inProcVel, parameter.accelSpeed * Time.fixedDeltaTime) / joycons.Count;
+
 		}
+		//加算した移動量を代入し、oldVelにvelを代入してMoveVelを初期化
+		rigidbody2D.velocity = parameter.moveVel;
+		parameter.ResetVel();
+
 	}
 #endregion
-
 }
